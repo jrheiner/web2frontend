d65fd02c09244e5f08992d795a69f7d29bbd45c1
Jonas Rheiner <39375864+jrheiner@users.noreply.github.com>
2020-11-28 12:47:47 +0100

    Update new post, add template-driven form and validation (incl. visual)

--- src/app/components/post-new/post-new.component.ts
+++ src/app/components/post-new/post-new.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder} from '@angular/forms';
 import {TokenService} from '../../services/token.service';
 import {ApiService} from '../../services/api.service';
 
@@ -9,17 +8,15 @@ import {ApiService} from '../../services/api.service';
   styleUrls: ['./post-new.component.scss']
 })
 export class PostNewComponent implements OnInit {
-  postForm;
-  info: string;
-  alert: string;
-  isLoading: boolean;
-  successId: string;
 
-  constructor(private formBuilder: FormBuilder, private session: TokenService, private apiService: ApiService) {
-    this.postForm = this.formBuilder.group({
-      title: '',
-      description: ''
-    });
+  post = {
+    title: '',
+    description: ''
+  };
+
+  successId = '0';
+
+  constructor(private session: TokenService, private apiService: ApiService) {
   }
 
   ngOnInit(): void {
@@ -28,29 +25,14 @@ export class PostNewComponent implements OnInit {
   createPost(data: { title: string, description: string }): void {
     console.log(data);
     this.apiService.postPost(data).subscribe(res => {
-      this.info = `Post ${res._id} successfully published.`;
       this.successId = res._id;
       console.log(res);
-      this.alert = '';
-      this.isLoading = false;
     }, err => {
       console.log(err);
-      this.isLoading = false;
-      this.postForm.enable();
-      this.alert = err.error.message;
     });
   }
 
-  onSubmit(data): void {
-    if (data.title !== '' && data.description !== '') {
-      this.isLoading = true;
-      this.postForm.disable();
-      this.createPost(data);
-    } else {
-      console.log(data);
-      this.alert = 'Invalid form';
-    }
+  onSubmit(): void {
+    this.createPost(this.post);
   }
-
-
 }
