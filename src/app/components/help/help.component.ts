b2514234303b51d41d49ed4472eeb12f9da6e723
Jonas Rheiner <39375864+jrheiner@users.noreply.github.com>
2020-11-29 13:57:21 +0100

    Add help content

--- src/app/components/help/help.component.ts
+++ src/app/components/help/help.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-help',
@@ -7,7 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelpComponent implements OnInit {
 
-  constructor() { }
+  helpDB = [
+    {
+      question: 'How can I create a post?',
+      answer: 'test'
+    },
+    {
+      question: 'How can I delete my account?',
+      answer: 'test1'
+    },
+    {
+      question: 'What are the username and password requirements?',
+      answer: 'test2'
+    }
+  ];
+
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
