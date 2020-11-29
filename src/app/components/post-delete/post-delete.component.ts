5c4b892b0985f87c4abf5ab6a0e371b9ae7c8544
Jonas Rheiner <39375864+jrheiner@users.noreply.github.com>
2020-11-29 20:24:13 +0100

    Add post delete component

--- src/app/components/post-delete/post-delete.component.ts
+++ src/app/components/post-delete/post-delete.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ApiService} from '../../services/api.service';
 
 @Component({
   selector: 'app-post-delete',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostDeleteComponent implements OnInit {
 
-  constructor() { }
+  private id = this.route.snapshot.paramMap.get('id');
+
+  constructor(private apiService: ApiService, private route: ActivatedRoute, private router: Router) {
+  }
 
   ngOnInit(): void {
   }
 
+  delete(): void {
+    this.apiService.deletePostById(this.id).subscribe(() => {
+      this.router.navigate(['/']);
+    });
+
+  }
+
+  cancel(): void {
+    this.router.navigate(['/post/' + this.id]);
+  }
 }
