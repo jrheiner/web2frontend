1160f9523db2f1a6c6cf71dcdccff428fd78950d
Jonas Rheiner <39375864+jrheiner@users.noreply.github.com>
2020-12-10 20:10:27 +0100

    Fix user-details last activity

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -3,52 +3,46 @@ import {ApiService} from '../../services/api.service';
 import {ActivatedRoute} from '@angular/router';
 
 @Component({
-    selector: 'app-user-details',
-    templateUrl: './user-details.component.html',
-    styleUrls: ['./user-details.component.scss']
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.scss']
 })
 export class UserDetailsComponent implements OnInit {
 
-    notFound;
-    user;
+  notFound;
+  user;
 
-    constructor(private apiService: ApiService, private route: ActivatedRoute) {
+  constructor(private apiService: ApiService, private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    const id: string = this.route.snapshot.paramMap.get('id');
+    if (id !== null) {
+      this.apiService.getUserById(id).subscribe((data) => {
+        this.setUserInfo(data);
+      }, err => {
+        this.notFound = err.error.message;
+        console.log(err);
+      });
+    } else {
+      this.apiService.getUserSelf().subscribe((data) => {
+        this.setUserInfo(data);
+      }, error => {
+        console.log(error);
+      });
     }
+  }
 
-    ngOnInit(): void {
-        const id: string = this.route.snapshot.paramMap.get('id');
-        if (id !== null) {
-            this.apiService.getUserById(id).subscribe((data) => {
-                this.user = data;
-                if (data.userActivity.posts[Object.keys(data.userActivity.posts).sort().reverse()[0]]
-                    || data.userActivity.comments[Object.keys(data.userActivity.comments).sort().reverse()[0]]) {
-                    this.user.lastAcitivty = data.userActivity.posts[Object.keys(data.userActivity.posts).sort().reverse()[0]].updatedAt
-                        || data.userActivity.comments[Object.keys(data.userActivity.comments).sort().reverse()[0]].updatedAt;
-                } else {
-                    this.user.lastAcitivty = data.createdAt;
-                }
-                this.user.postCount = Object.keys(data.userActivity.posts).length;
-                this.user.commentCount = Object.keys(data.userActivity.comments).length;
-            }, err => {
-                this.notFound = err.error.message;
-                console.log(err);
-            });
-        } else {
-            this.apiService.getUserSelf().subscribe((data) => {
-                this.user = data;
-                if (data.userActivity.posts[Object.keys(data.userActivity.posts).sort().reverse()[0]]
-                    || data.userActivity.comments[Object.keys(data.userActivity.comments).sort().reverse()[0]]) {
-                    this.user.lastAcitivty = data.userActivity.posts[Object.keys(data.userActivity.posts).sort().reverse()[0]].updatedAt
-                        || data.userActivity.comments[Object.keys(data.userActivity.comments).sort().reverse()[0]].updatedAt;
-                } else {
-                    this.user.lastAcitivty = data.createdAt;
-                }
-                this.user.postCount = Object.keys(data.userActivity.posts).length;
-                this.user.commentCount = Object.keys(data.userActivity.comments).length;
-
-            }, error => {
-                console.log(error);
-            });
-        }
+  setUserInfo(data: any): void {
+    this.user = data;
+    if (data.userActivity.posts.length !== 0) {
+      this.user.lastAcitivty = data.userActivity.posts[Object.keys(data.userActivity.posts).sort().reverse()[0]].updatedAt;
+    } else if (data.userActivity.comments.length !== 0) {
+      this.user.lastAcitivty = data.userActivity.comments[Object.keys(data.userActivity.comments).sort().reverse()[0]].updatedAt;
+    } else {
+      this.user.lastAcitivty = data.createdAt;
     }
+    this.user.postCount = Object.keys(data.userActivity.posts).length;
+    this.user.commentCount = Object.keys(data.userActivity.comments).length;
+  }
 }
